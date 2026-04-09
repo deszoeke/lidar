8682c98985b6e4c555ff4fbf67ea6f0682602c2b
Simon de Szoeke <simon.deszoeke@oregonstate.edu>
2026-04-09 15:33:49 -0700

    3 more PyCall.PyObject method extensions

--- ASTRAL2024/save_lidar_dt.jl
+++ ASTRAL2024/save_lidar_dt.jl
@@ -22,7 +22,7 @@ using PyCall: PyObject
 # PyObject method interprets Array{Union{T,Missing}} as a
 # numpy masked array.
 # This allows for plotting with missing values.
-function PyObject(a::Array{Union{T,Missing},N}) where {T,N}
+function PyCall.PyObject(a::Array{Union{T,Missing},N}) where {T,N}
     numpy_ma = PyCall.pyimport("numpy").ma
     pycall(numpy_ma.array, Any, coalesce.(a,zero(T)), mask=ismissing.(a))
 end
